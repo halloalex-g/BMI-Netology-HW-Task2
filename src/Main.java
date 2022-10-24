4a65361010800e7b6d172e2269b4d81bbba204f4
Aleksey Kaschenko <aleksey.kaschenko@gmail.com>
2022-10-24 10:45:25 +0600

    Output format added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
+import java.text.DecimalFormat;
+
 public class Main {
+    static double mass = 80;
+    static double height = 1.8;
+
     public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("#.##");
         BmiService service = new BmiService();
-        double mass = 80;
-        double height = 1.8;
+
         double bodyMassIndex = service.calculate(mass, height);
-        System.out.println(bodyMassIndex);
+        System.out.println(df.format(bodyMassIndex));
+
     }
+
 }
